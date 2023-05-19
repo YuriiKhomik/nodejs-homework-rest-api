456efa5cd6eebb671fd6bb85a0237e345ea0df4a
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-05-19 22:24:32 +0300

    added pagination

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,11 +1,15 @@
-// const contacts = require("../models/contacts");
 const { Contact } = require("../models/contacts/contact");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res, next) => {
   const { _id: owner } = req.user;
-  const result = await Contact.find({ owner });
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "email");
   res.json(result);
 };
 
