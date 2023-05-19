9c8556e668cd48355a7c41f656d73f6936b15fe2
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-05-19 19:34:22 +0300

    added bcrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,11 @@
+const bcrypt = require("bcrypt");
+
 const { User } = require("../models/contacts/user");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const register = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
 
   const user = await User.findOne({ email });
 
@@ -11,7 +13,9 @@ const register = async (req, res) => {
     throw HttpError(409, "Email in use");
   }
 
-  const newUser = await User.create(req.body);
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({ ...req.body, password: hashPassword });
 
   res.status(201).json({
     user: { email: newUser.email, subscription: newUser.subscription },
